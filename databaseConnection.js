8a9c7466b65589403d5ac167e03c2474092b119e
gnagetEmil <emil@gnaget.se>
2024-02-23 15:29:42 +0100

    Added functionality for adding user to database

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,53 +1,25 @@
-const firebase = require('firebase/app');
-require('firebase/auth');
-require('firebase/database');
+const admin = require('firebase-admin');
 
-// Initialize Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyBMbxHDxnmklkCuAVybMwdkIu1Xvd7s5zw",
-  authDomain: "smart-home-7ba6c.firebaseapp.com",
-  databaseURL: "https://smart-home-7ba6c-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "smart-home-7ba6c",
-  storageBucket: "smart-home-7ba6c.appspot.com",
-  messagingSenderId: "789831944832",
-  appId: "1:789831944832:web:ebbc9835251ec3cc9af465",
-  measurementId: "G-H8839RQRCM"
-};
+// Initialize Firebase Admin with service account
+const serviceAccount = require('./firebase-adminsdk.json');
 
-firebase.initializeApp(firebaseConfig);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
-
-
-// Function for handling log in
-async function signInWithEmailAndPassword(email, password) {
-    try {
-        const userCredential = await firebase.auth().signInWithEmailAndPassword(email, password);
-        return userCredential.user;
-    } catch (error) {
-        throw error;
-    }
+// Function to create a new user
+async function createUserWithEmailAndPassword(email, password) {
+  try {
+    const userRecord = await admin.auth().createUser({
+      email: email,
+      password: password,
+    });
+    console.log('Successfully created new user:', userRecord.uid);
+    return userRecord;
+  } catch (error) {
+    console.error('Error creating new user:', error);
+    throw error; // Rethrow the error so it can be caught by the calling function
+  }
 }
 
-
-// Function to create a new user with email and password
-async function createUserWithEmailAndPassword(email, password) {
-    try {
-      const userCredential = await firebase.auth().createUserWithEmailAndPassword(email, password);
-      return userCredential.user;
-    } catch (error) {
-      throw error;
-    }
-  }
-
-
-// Access Firebase Realtime Database
-//const database = firebase.database();
-//const ref = database.ref('path/to/your/data');
-
-// Read data
-/*ref.once('value', (snapshot) => {
-  const data = snapshot.val();
-  console.log(data);
-});*/
-
-module.exports = { signInWithEmailAndPassword, createUserWithEmailAndPassword }
+module.exports = { createUserWithEmailAndPassword };
