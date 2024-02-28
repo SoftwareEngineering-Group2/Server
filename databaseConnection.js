685ac9268fa50909059ea9671c6980313a87ec99
gnagetEmil <emil@gnaget.se>
2024-02-28 18:31:17 +0100

    Added API routes for yellow/white led images

--- databaseConnection.js
+++ databaseConnection.js
@@ -11,8 +11,10 @@ admin.initializeApp({
 const db = admin.database();
 
 // Strings for database refences
-const whiteLedDeviceState = "devices/0/deviceState";
-const yellowLedDeviceState = "devices/1/deviceState"
+const whiteLedDeviceStateRef = "devices/0/deviceState";
+const whiteLedImageUrlRef = "devices/0/deviceIcon";
+const yellowLedDeviceStateRef = "devices/1/deviceState";
+const yellowLedImageUrlRef = "devices/1/deviceIcon"
 
 const updateWhiteLed = async (state) => {
   try {
@@ -27,7 +29,7 @@ const updateWhiteLed = async (state) => {
 
 const readWhiteLedState = async () => {
   try {
-    const ref = db.ref(whiteLedDeviceState);
+    const ref = db.ref(whiteLedDeviceStateRef);
     const snapshot = await ref.once('value'); // Use once('value') to read the data at the path
     const state = snapshot.val(); // Extracting the state value from the snapshot
     console.log(`White LED state is: ${state}`);
@@ -38,9 +40,22 @@ const readWhiteLedState = async () => {
   }
 };
 
+const readWhiteLedImage = async () =>{
+  try{
+    const ref = db.ref(whiteLedImageUrlRef);
+    const snapshot = await ref.once('value');
+    const imageUrl = snapshot.val();
+    console.log(`White LED imageUrl is: ${imageUrl}`);
+    return imageUrl;
+  }catch(error){
+    console.error('Error reading the white LED state:', error);
+    throw error; // Rethrow the error so it can be handled by the caller
+  }
+}
+
 const updateYellowLed = async (state) => {
   try {
-    const ref = db.ref(yellowLedDeviceState);
+    const ref = db.ref(yellowLedDeviceStateRef);
     await ref.set(state);
     console.log(`yellow LED state updated to: ${state}`);
   } catch (error) {
@@ -51,7 +66,7 @@ const updateYellowLed = async (state) => {
 
 const readYellowLedState = async () => {
   try {
-    const ref = db.ref(yellowLedDeviceState);
+    const ref = db.ref(yellowLedDeviceStateRef);
     const snapshot = await ref.once('value'); // Use once('value') to read the data at the path
     const state = snapshot.val(); // Extracting the state value from the snapshot
     console.log(`White LED state is: ${state}`);
@@ -62,4 +77,17 @@ const readYellowLedState = async () => {
   }
 };
 
-module.exports = { updateWhiteLed, readWhiteLedState, updateYellowLed, readYellowLedState };
+const readYellowLedImage = async () =>{
+  try{
+    const ref = db.ref(yellowLedImageUrlRef);
+    const snapshot = await ref.once('value');
+    const imageUrl = snapshot.val();
+    console.log(`Yellow LED imageUrl is: ${imageUrl}`);
+    return imageUrl;
+  }catch(error){
+    console.error('Error reading the white LED state:', error);
+    throw error; // Rethrow the error so it can be handled by the caller
+  }
+}
+
+module.exports = {updateWhiteLed, readWhiteLedState,readWhiteLedImage, updateYellowLed, readYellowLedState, readYellowLedImage};
--- server.js
+++ server.js
@@ -1,48 +1,66 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const { updateWhiteLed, readWhiteLedState, updateYellowLed, readYellowLedState } = require('./databaseConnection');
+const { updateWhiteLed, readWhiteLedState, readWhiteLedImage, updateYellowLed, readYellowLedState, readYellowLedImage } = require('./databaseConnection');
 
 // Middleware
 app.use(express.json()); // for parsing application/json
 
 app.post('/whiteLed', async (req, res) => {
-    const { state } = req.body; 
-    try {
-        await updateWhiteLed(state);
-        res.json({ message: `Lamp turned ${state}` });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+  const { state } = req.body; 
+  try {
+      await updateWhiteLed(state);
+      res.json({ message: `Lamp turned ${state}` });
+  } catch (error) {
+      res.status(500).json({ error: error.message });
+  }
 });
 
 app.get('/whiteLed', async (req, res) => {
-    try {
-      const state = await readWhiteLedState();
-      res.json({ state: state, message: `White LED is currently ${state}` });
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  });
+  try {
+    const state = await readWhiteLedState();
+    res.json({ state: state, message: `White LED is currently ${state}` });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get('/whiteLedImage', async (req,res) => {
+  try {
+    const imageUrl = await readWhiteLedImage();
+    res.json({imageUrl: imageUrl});
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+})
 
 app.post('/yellowLed', async (req, res) => {
-    const { state } = req.body;
-    try {
-        await updateYellowLed(state);
-        res.json({ message: `Lamp turned ${state}` });
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+  const { state } = req.body;
+  try {
+      await updateYellowLed(state);
+      res.json({ message: `Lamp turned ${state}` });
+  } catch (error) {
+      res.status(500).json({ error: error.message });
+  }
 });
 
 app.get('/yellowLed', async (req, res) => {
-    try {
-      const state = await readYellowLedState();
-      res.json({ state: state });
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  });
+  try {
+    const state = await readYellowLedState();
+    res.json({ state: state });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get('/yellowLedImage', async (req,res) => {
+  try {
+    const imageUrl = await readYellowLedImage();
+    res.json({imageUrl: imageUrl});
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
